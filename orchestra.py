83198500582750fb67202406983f4e20694b6098
sergey.kudasov <sergey.kudasov@rtlabs.ru>
2017-02-17 18:39:52 +0300

    stable build of a single package

--- orchestra.py
+++ orchestra.py
@@ -60,63 +60,53 @@ def get_stdout_exec(command, comment, check=None):
 
 def make_deb_chroot(apt_repo):
     if args.wipe:
-        host_exec(command='sudo rm -rf /stable-temp-jail',
-                  comment='Wiping environment in /stable-temp-jail...',
-                  check=None)
+        host_exec(command='sudo umount %s%s' % (JAIL_PATH, SHARED_DIR),
+                  comment='Unmount shared dir...',)
 
-    host_exec(command='sudo debootstrap stable /stable-temp-jail %s' % apt_repo,
-              comment='Creating jail from repo...',
-              check=None)
+        host_exec(command='sudo rm -rf /stable-temp-jail',
+                  comment='Wiping environment in /stable-temp-jail...',)
+
+    host_exec(command='sudo debootstrap stable /stable-temp-jail http://deb.debian.org/debian/',
+              comment='Creating jail from repo...',)
 
     jail_exec(command='echo "127.0.0.1   %s" >> /etc/hosts' % apt_repo,
-              comment='Adding vhost repo to hosts...',
-              check=None)
+              comment='Adding vhost repo to hosts...',)
 
     # For testing purposes repository provided for jail and repository for rebuilded changes is not the same thing
     jail_exec(command='wget -O - -q http://%s/apt.example.com.gpg.key | apt-key add -' % apt_repo,
-              comment='Getting pgp key...',
-              check=None)
+              comment='Getting pgp key...',)
 
     jail_exec(command='echo "deb http://%s/debian testing main" >> /etc/apt/sources.list' % apt_repo,
-              comment='Adding repo to source.list...',
-              check=None)
+              comment='Adding repo to source.list...',)
 
     jail_exec(command='echo "deb-src http://%s/debian testing main" >> /etc/apt/sources.list' % apt_repo,
-              comment='Adding repo to source.list...',
-              check=None)
+              comment='Adding repo to source.list...',)
+
+    jail_exec(command="apt-get update",
+              comment='Updating repos...',)
 
     jail_exec(command='''cat <<EOT >> /etc/apt/preferences.d/preferences
 Package: *
 Pin: origin %s
 Pin-Priority: 1001
 EOT''' % apt_repo,
-              comment='',
-              check=None)
+              comment='',)
 
     jail_exec(command='mkdir -p %s' % PKG_RDY_DIR,
-              comment='Making packages dir...',
-              check=None)
+              comment='Making packages dir...',)
 
     jail_exec(command='mkdir -p %s' % SHARED_DIR,
-              comment='Making shared dir from chroot to host...',
-              check=None)
+              comment='Making shared dir from chroot to host...',)
 
     jail_exec(command='apt-get install build-essential devscripts -y',
-              comment='Installing required build-deps',
-              check=None)
+              comment='Installing required build-deps',)
 
     jail_exec(command='export LANGUAGE="C"; export LC_ALL="C"',
-              comment='Installing required build-deps',
-              check=None)
-
-    jail_exec(command="apt-get update",
-              comment='Updating repos...',
-              check=None)
+              comment='Installing required build-deps',)
 
     # TODO: Here data is lost, umount from jail needed?
-    # host_exec(command='sudo mount --bind %s %s%s' % (SHARED_DIR, '/stable-temp-jail', SHARED_DIR),
-    #           comment='Mounting dir for reprepro...',
-    #           check=None)
+    host_exec(command='sudo mount --bind %s %s%s' % (SHARED_DIR, '/stable-temp-jail', SHARED_DIR),
+              comment='Mounting dir for reprepro...',)
 
 
 def rebuild_package(pkg, wipe=False):
@@ -125,24 +115,19 @@ def rebuild_package(pkg, wipe=False):
                   comment='Removing rebuild directory...')
 
     jail_exec(command='mkdir -p %s' % REBUILD_DIR,
-              comment='Creating home dir for packages rebuild...',
-              check=None)
+              comment='Creating home dir for packages rebuild...',)
 
     jail_exec(command='cd %s && apt-get source %s' % (REBUILD_DIR, pkg),
-              comment='Download package source...',
-              check=None)
+              comment='Download package source...',)
 
     jail_exec(command='cd %s*' % pkg,
-              comment='Cd to pkg dir %s*...' % pkg,
-              check=None)
+              comment='Cd to pkg dir %s*...' % pkg,)
 
     jail_exec(command='cd %s%s* && apt-get build-dep %s -y' % (REBUILD_DIR, pkg, pkg),
-              comment='Get build-deps...',
-              check=None)
+              comment='Get build-deps...',)
 
     jail_exec(command='cd %s%s* && debuild -us -uc' % (REBUILD_DIR, pkg),
-              comment='Rebuilding...',
-              check=None)
+              comment='Rebuilding...',)
 
     # from host
     jail_exec(command='cd %s && cp %s* %s' % (REBUILD_DIR, pkg, SHARED_DIR),
@@ -171,9 +156,11 @@ def get_deps_list(pkg_list):
         dep_list = get_stdout_exec(command='apt-cache depends %s | grep Depends' % p,
                                    comment='Getting list of dependencies for %s' % p)
         dep_list = dep_list.split('\n')
+
         # stripping and splitting if el is not empty
         dep_list = [el.split(':')[1].strip() for el in dep_list if el]
         print dep_list
+
 
 if __name__ == '__main__':
     args = parser.parse_args()
